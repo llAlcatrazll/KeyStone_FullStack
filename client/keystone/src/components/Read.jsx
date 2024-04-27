262eacf29672ddc92f766d15e5696f7ee818c5cb
llAlcatrazll <alexismagaway456@gmail.com>
2024-04-27 21:13:46 +0800

    Added Editing and Displaying

--- client/keystone/src/components/Read.jsx
+++ client/keystone/src/components/Read.jsx
@@ -1,5 +1,56 @@
+import { Link, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 function Read() {
-  return <div>Read</div>;
+  const [data, setData] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/get_booking/${id}`)
+      // modify for the whole URL to avoid navigation confusion
+      .then((res) => {
+        // console.log(res.data); // Add this line to inspect the response
+        setData(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, [id]);
+  return (
+    <div className="container-fluid vw-100 vh-100 bg-primary">
+      <h1>User {id}</h1>
+      <Link to="/" className="btn btn-success">
+        Back
+      </Link>
+      <h1>User Details</h1>
+      {Array.isArray(data) &&
+        data.map((student, index) => {
+          return (
+            <ul key={index} className="list-group">
+              <li>
+                <b>ID: </b>
+                {student["id"]}
+              </li>
+              <li>
+                <b>Name: </b>
+                {student["name"]}
+              </li>
+              <li>
+                <b>Email: </b>
+                {student["email"]}
+              </li>
+              <li>
+                <b>Age: </b>
+                {student["age"]}
+              </li>
+              <li>
+                <b>Gender: </b>
+                {student["gender"]}
+              </li>
+            </ul>
+          );
+        })}
+    </div>
+  );
 }
 
 export default Read;
--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
 const path = require("path");
-
 const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
@@ -10,14 +9,14 @@ app.use(cors());
 app.use(express.json());
 
 const port = 5000;
-
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
   database: "booking",
 });
-
+//        /add_user
+//  CREATE BOOKING
 app.post("/add_user", (req, res) => {
   sql =
     "INSERT INTO user_details (`name`,`email`,`age`,`gender`)VALUES (?, ?, ?, ?)";
@@ -29,6 +28,8 @@ app.post("/add_user", (req, res) => {
   });
 });
 
+//       /students
+//  DISPLAY ALL BOOKINGS
 app.get("/booking", (req, res) => {
   const sql = "SELECT * FROM user_details";
   db.query(sql, (err, result) => {
@@ -36,7 +37,43 @@ app.get("/booking", (req, res) => {
     return res.json(result);
   });
 });
+//
+//
+//       /get_student
+//  DISPLAY BOOKING DETAILS
+app.get("/get_booking/:id", (req, res) => {
+  const id = req.params.id;
+  const sql = "SELECT * FROM user_details WHERE `id`= ?";
+  db.query(sql, [id], (err, result) => {
+    if (err) res.json({ message: "Server error" });
+    return res.json(result);
+  });
+});
+//
+//
+//
+//   EDIT BOOKING DETAILS - ADMIN ONLY
+app.post("/edit_user/:id", (req, res) => {
+  const id = req.params.id;
+  const sql =
+    "UPDATE user_details SET `name`=?, `email`=?, `age`=?, `gender`=? WHERE id=?";
+  const values = [
+    req.body.name,
+    req.body.email,
+    req.body.age,
+    req.body.gender,
+    id,
+  ];
+  console.log(values);
+  db.query(sql, values, (err, result) => {
+    if (err)
+      return res.json({ message: "Something unexpected has occured" + err });
+    return res.json({ success: "Student updated successfully" });
+  });
+});
 
+//
+//
 app.listen(port, () => {
   console.log("listening");
 });
